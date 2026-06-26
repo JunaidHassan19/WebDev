66158808d9b7e7d4af38e8b0c4406348a1174cba
JunaidHassan19 <thejunni01@gmail.com>
2026-06-26 12:04:59 +0530

    Linking JS File

---
+++ 05-javascript/JavaScript (Part 2)/linking.js
@@ -0,0 +1,7 @@
+console.log("Hello from linking.js!");
+
+let a = 10;
+let b = 20;
+console.log("The sum of a and b is: ", a + b);
+
+console.log("This is a simple JavaScript file that demonstrates linking and basic operations.");
