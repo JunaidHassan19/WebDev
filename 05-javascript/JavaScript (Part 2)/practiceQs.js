f5c52d68381c3c7ec5afbbe9862cfe6662897af3
JunaidHassan19 <thejunni01@gmail.com>
2026-06-26 23:44:11 +0530

    Practice Qs

--- 05-javascript/JavaScript (Part 2)/practiceQs.js
+++ 05-javascript/JavaScript (Part 2)/practiceQs.js
@@ -12,3 +12,17 @@ if(light === 'green'){
   console.log("Go");
 }
 
+// Qs: Create a system to calculate popcorn price based on the size customer asked for
+
+let size = 'M';
+
+if(size === 'XL'){
+  console.log("Price is Rs. 250")
+} else if(size === 'L') {
+    console.log("Price is Rs. 200")
+} else if(size === 'M') {
+    console.log("Price is Rs. 100")
+} else if(size ==='S') {
+    console.log("Price is Rs. 50")
+
+}
