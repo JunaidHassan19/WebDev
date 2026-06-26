8eca295b9fe5128afb0aa78569e082ba89f5bfe3
JunaidHassan19 <thejunni01@gmail.com>
2026-06-26 21:05:19 +0530

    Operators in JS

--- 05-javascript/JavaScript (Part 2)/operators.js
+++ 05-javascript/JavaScript (Part 2)/operators.js
@@ -11,6 +11,16 @@ console.log(x ** y); // Exponentiation
 console.log(++x); // pre-increment
 console.log(x++); // post-increment
 
+// Comparison Operators
+console.log(x == y); // Equal to
+console.log(x != y); // Not equal to
+console.log(x === y); // Strict equal to
+console.log(x !== y); // Strict not equal to
+console.log(x > y); // Greater than
+console.log(x < y); // Less than
+console.log(x >= y); // Greater than or equal to
+console.log(x <= y); // Less than or equal to
+
 
 // Logical Operators
 console.log(x > 0 && y > 0); // Logical AND
