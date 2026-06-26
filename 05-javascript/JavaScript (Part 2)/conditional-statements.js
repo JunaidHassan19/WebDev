bef6fa7efdc7fd86a5d370ea52a043f025e409d9
JunaidHassan19 <thejunni01@gmail.com>
2026-06-26 23:27:28 +0530

    if-else statement

--- 05-javascript/JavaScript (Part 2)/conditional-statements.js
+++ 05-javascript/JavaScript (Part 2)/conditional-statements.js
@@ -1,8 +1,8 @@
 // ============== if statement ================
 
-if (condition) {
-  // code to execute if condition is true
-}
+// if (condition) {
+//   // code to execute if condition is true
+// }
 
 let age = 20;
 
@@ -10,3 +10,33 @@ if (age >= 18) {
   console.log("Adult");
 }
 
+// ============== if-else statement ================
+
+//  if(condition1) {
+//   // code to execute if condition1 is true
+// } else if (condition2) {
+//   // code to execute if condition2 is true
+// } 
+
+let age1 = 15;
+
+if (age1 >= 18) {
+  console.log("Adult");
+} else if (age1 >= 13) {
+  console.log("Teenager");
+} else {
+  console.log("Child");
+}
+
+let marks = 46;
+if(marks >= 98) {
+  console.log("S")
+} else if(marks >=90) {
+  console.log("A");
+}else if(marks >= 70) {
+  console.log("B");
+}else if(marks >= 50) {
+  console.log("C");
+}else{
+  console.log("F")
+}
