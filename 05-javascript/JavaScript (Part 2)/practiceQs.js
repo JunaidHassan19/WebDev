943d15ab9d7501d1c76029d0b90c0a655ec5c176
JunaidHassan19 <thejunni01@gmail.com>
2026-06-26 23:14:34 +0530

    Practice Qs

---
+++ 05-javascript/JavaScript (Part 2)/practiceQs.js
@@ -0,0 +1,14 @@
+// Qs: Create a traffic system that shows what to do based on color
+
+let light = "green";
+
+if(light === 'red'){
+  console.log("Stop");
+}
+if(light ==='yellow'){
+  console.log("Slow down");
+}
+if(light === 'green'){
+  console.log("Go");
+}
+
