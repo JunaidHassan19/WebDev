d75a589d751b83cfb77d2d420fcd2a3780be3c15
JunaidHassan19 <thejunni01@gmail.com>
2026-06-26 21:03:16 +0530

    Operators in JS

---
+++ 05-javascript/JavaScript (Part 2)/operators.js
@@ -0,0 +1,28 @@
+let x  = 5;
+let y = 10;
+
+// Arithmetic Operators
+console.log(x + y); // Addition
+console.log(x - y); // Subtraction
+console.log(x * y); // Multiplication
+console.log(x / y); // Division
+console.log(x % y); // Modulus
+console.log(x ** y); // Exponentiation
+console.log(++x); // pre-increment
+console.log(x++); // post-increment
+
+
+// Logical Operators
+console.log(x > 0 && y > 0); // Logical AND
+console.log(x > 0 || y < 0); // Logical OR
+console.log(!(x > 0)); // Logical NOT
+
+// Assignment Operators
+x += 5;
+console.log(x); // 10
+x -= 2;
+console.log(x); // 8
+x *= 3;
+console.log(x); // 24
+x /= 4;
+console.log(x); // 6
