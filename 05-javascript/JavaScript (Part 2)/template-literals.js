4eea6d2fbf3d734c9b4b5378df71d1f88e0b95b4
JunaidHassan19 <thejunni01@gmail.com>
2026-06-26 12:20:38 +0530

    Template literals

---
+++ 05-javascript/JavaScript (Part 2)/template-literals.js
@@ -0,0 +1,8 @@
+let penPrice = 10;
+let pencilPrice = 5;
+
+let totalCost = `The total cost of the pen and pencil is ${penPrice + pencilPrice} rupees.`;
+console.log(totalCost);
+console.log(`Template literals allow for multi-line strings as well:
+This is the first line.
+This is the second line.`);
