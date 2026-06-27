b863e02368d7406250442d13d9552070c30c173a
JunaidHassan19 <thejunni01@gmail.com>
2026-06-27 11:23:56 +0530

    Alerts Prompts

---
+++ 05-javascript/JavaScript (Part 2)/alerts-prompts.js
@@ -0,0 +1,10 @@
+// alert("Hello, World!"); 
+console.log("Hello, World!");
+console.error("This is an error message");
+console.warn("This is a warning message");
+
+let fristNme = prompt("Enter your firstName?");
+let lastName = prompt("Enter your lastName?");
+let fullName = fristNme + " " + lastName;
+alert("Hello, " + fullName + "!");
+console.log("Hello, " + fullName + "!");
