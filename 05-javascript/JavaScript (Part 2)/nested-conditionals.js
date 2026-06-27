3ec7172f5d67286b09d02c71669d2d43a6410c33
JunaidHassan19 <thejunni01@gmail.com>
2026-06-27 10:20:03 +0530

    Nested Conditionals

---
+++ 05-javascript/JavaScript (Part 2)/nested-conditionals.js
@@ -0,0 +1,19 @@
+let marks = 65;
+
+if(marks >= 34) {
+  if(marks >= 60) {
+    if(marks >= 80) {
+      if(marks >= 95) {
+        console.log("S");
+      }else{
+        console.log("A+");
+      }
+    } else{
+      console.log("A");
+    }
+  }else{
+    console.log("B");
+  }
+}else{
+  console.log("F");
+}
