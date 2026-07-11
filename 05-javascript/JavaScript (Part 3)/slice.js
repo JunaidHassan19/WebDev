4a157b637d5d9d1f803e5ef6b0092cf07ed5c535
JunaidHassan19 <thejunni01@gmail.com>
2026-07-11 10:56:17 +0530

    Slice Methods

---
+++ 05-javascript/JavaScript (Part 3)/slice.js
@@ -0,0 +1,5 @@
+let str = "hey there, how are you?";
+console.log(str.slice(4)); // "there, how are you?"
+console.log(str.slice(0, 3)); // "hey"
+console.log(str.slice(-3)); // "you"
+console.log(str.slice(-12)); // "how are you?"
