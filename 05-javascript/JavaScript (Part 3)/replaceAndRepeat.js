0d591a20fda02d462a26ece515acd63e38d44483
JunaidHassan19 <thejunni01@gmail.com>
2026-07-11 11:02:26 +0530

    Replace and Repeat Method

---
+++ 05-javascript/JavaScript (Part 3)/replaceAndRepeat.js
@@ -0,0 +1,4 @@
+let str = "hello world, welcome to the world of JavaScript.";
+console.log(str.replace("world", "universe")); // "hello universe, welcome to the world of JavaScript."
+console.log(str.replaceAll("world", "universe")); // "hello universe, welcome to the universe of JavaScript."
+console.log(str.repeat(2)); // "hello world, welcome to the world of JavaScript.hello world, welcome to the world of JavaScript."
