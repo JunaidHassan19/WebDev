cde97bc188e480547f2510a78bdc9beff7cd8b51
JunaidHassan19 <thejunni01@gmail.com>
2026-07-11 09:43:37 +0530

    Trim Methods

---
+++ 05-javascript/JavaScript (Part 3)/trim.js
@@ -0,0 +1,8 @@
+let name = "  Junaid Hassan  ";
+console.log(name.trim()); // "Junaid Hassan"
+console.log(name.trimStart()); // "Junaid Hassan  "
+console.log(name.trimEnd()); // "  Junaid Hassan"
+console.log(name.length); // 13
+
+let password = prompt("Enter Password: ");
+console.log(password.trim());
