0d3c44b6849b081007024973ecaf2a1c44659c23
JunaidHassan19 <thejunni01@gmail.com>
2026-07-11 11:17:58 +0530

    Practice Qs

---
+++ 05-javascript/JavaScript (Part 3)/practiceQs.js
@@ -0,0 +1,9 @@
+// Qs 1: for the given string, trim it and convert it to uppercase.
+
+let str1 = " help! "
+console.log(str1.trim().toUpperCase());
+          
+
+// Qs 2: saparate the "college" part in above string and replace "i" with " t" in it.
+let str2 = "ApnaCollege";
+console.log(str2.slice(4).replaceAll("l", "t"));
