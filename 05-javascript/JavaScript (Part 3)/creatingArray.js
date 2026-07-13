cc7f3b98af8c702a1be85521f4d5e664c6d0099a
JunaidHassan19 <thejunni01@gmail.com>
2026-07-13 09:56:15 +0530

    Creating Arrays

---
+++ 05-javascript/JavaScript (Part 3)/creatingArray.js
@@ -0,0 +1,14 @@
+let arr = [1, 2, 3, 4, 5];
+console.log(arr);
+
+let arr2 = ["hello", 20, 2.5, true, null];
+console.log(arr2);
+
+console.log(arr2[0]); // hello
+console.log(arr2[1]); // 20
+console.log(arr2[2]); // 2.5
+console.log(arr2[3]); // true
+console.log(arr2[4]); // null
+
+
+console.log(arr2[0][0]); // h  first character of hello
