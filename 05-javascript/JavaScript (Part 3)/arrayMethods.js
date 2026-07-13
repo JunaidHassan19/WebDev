fe6f34a04ca0c941e38c109a72f3a4f5339702a4
JunaidHassan19 <thejunni01@gmail.com>
2026-07-13 10:22:51 +0530

    Array Methods

---
+++ 05-javascript/JavaScript (Part 3)/arrayMethods.js
@@ -0,0 +1,14 @@
+let cars = ["BMW", "Mercedes", "Audi", "Toyota", "Honda"];
+console.log(cars);
+
+cars.push("Nissan"); // Adds "Nissan" to the end of the array
+console.log(cars);
+
+cars.pop(); // Removes the last element from the array
+console.log(cars);
+
+cars.unshift("Kia"); // Adds "Kia" to the beginning of the array
+console.log(cars);
+
+cars.shift(); // Removes the first element from the array
+console.log(cars);
