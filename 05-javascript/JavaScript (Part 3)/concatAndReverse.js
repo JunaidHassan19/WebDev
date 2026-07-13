003901a3e2e7537d1e3f8b1dd8045b8e5b984687
JunaidHassan19 <thejunni01@gmail.com>
2026-07-13 10:46:54 +0530

    Concatenation And Reverse

---
+++ 05-javascript/JavaScript (Part 3)/concatAndReverse.js
@@ -0,0 +1,7 @@
+let arr1 = ["Junaid", "Hassan"];
+let arr2 = ["is", "a", "good", "boy"];
+let arr3 = arr1.concat(arr2);
+console.log(arr3); // ["Junaid", "Hassan", "is", "a", "good", "boy"]
+
+arr3.reverse();
+console.log(arr3); // ["boy", "good", "a", "is", "Hassan", "Junaid"]
