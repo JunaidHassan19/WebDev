50c9624a704874dc4295843d9c2dd3fb8c432975
JunaidHassan19 <thejunni01@gmail.com>
2026-07-13 11:03:35 +0530

    Sort in Arrays

---
+++ 05-javascript/JavaScript (Part 3)/sort.js
@@ -0,0 +1,7 @@
+let nums = [2, 6, 5, 9, 1, 3, 4, 8, 7];
+console.log("Before sorting:", nums);
+console.log("After sorting:", nums.sort());
+
+let months = ["March", "Jan", "Feb", "Dec", "Nov", "Oct"];
+console.log("Before sorting:", months);
+console.log("After sorting:", months.sort());
