ffd154d4da96d2f2e612167cb17eb07f20b8055c
JunaidHassan19 <thejunni01@gmail.com>
2026-07-13 10:39:07 +0530

    Practice Qs

--- 05-javascript/JavaScript (Part 3)/practiceQs.js
+++ 05-javascript/JavaScript (Part 3)/practiceQs.js
@@ -15,4 +15,9 @@ let months = ["january", "july", "march", "august"]
 console.log(months);
 
 months.shift();
+months.shift();
+console.log(months);
+
+months.unshift("june");
+months.unshift("july");
 console.log(months);
