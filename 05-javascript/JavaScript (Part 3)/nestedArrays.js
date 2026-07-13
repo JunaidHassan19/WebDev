2f560bd3a51360bb58673455c8989923ebb9fd22
JunaidHassan19 <thejunni01@gmail.com>
2026-07-13 11:32:42 +0530

    Nested Arrays

---
+++ 05-javascript/JavaScript (Part 3)/nestedArrays.js
@@ -0,0 +1,11 @@
+let nums = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+console.log(nums);
+
+console.log(nums.length); // 3
+console.log(nums[0]); // [1, 2, 3]
+console.log(nums[0].length); // 3
+
+
+console.log(nums[0][0]); // 1
+console.log(nums[0][1]); // 2
+console.log(nums[0][2]); // 3
