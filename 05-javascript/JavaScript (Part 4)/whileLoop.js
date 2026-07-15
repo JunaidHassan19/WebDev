b4222e764349573c9add128830ae564caaf2efef
JunaidHassan19 <thejunni01@gmail.com>
2026-07-15 20:34:28 +0530

    while loop

---
+++ 05-javascript/JavaScript (Part 4)/whileLoop.js
@@ -0,0 +1,16 @@
+/*
+
+while loop is used to execute a block of code repeatedly as long as a specified condition is true. The syntax of a while loop is as follows:
+
+initialization;
+while (condition) {
+  // code block to be executed
+}
+
+*/
+
+let i = 0;
+while(i < 10) {
+  console.log(i);
+  i++;
+}
