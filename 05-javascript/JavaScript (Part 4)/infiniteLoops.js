0a6449454c807984d27e94a4bd784200a5360847
JunaidHassan19 <thejunni01@gmail.com>
2026-07-15 10:49:47 +0530

    Infinite loops

---
+++ 05-javascript/JavaScript (Part 4)/infiniteLoops.js
@@ -0,0 +1,21 @@
+/*
+
+Infinite loops run forever unless they are stopped manually or with a break.
+
+*/
+
+// This will run infinitely because i is decremented but the condition checks for i < 10
+for(let i = 0; i < 10; i--) {
+  console.log(i);
+}
+
+
+// This will run infinitely because the condition is always true
+for(let i = 1; i >=5; i++) {
+  console.log(i);
+}
+
+// This will run infinitely because there is no condition to stop the loop
+for(let i = 1;  ; i ++) {
+  console.log(i);
+}
