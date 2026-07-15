6cd2d3b09de1f0cd4886f8d873b5465d70804114
JunaidHassan19 <thejunni01@gmail.com>
2026-07-15 10:17:26 +0530

    Print Odd Numbers

---
+++ 05-javascript/JavaScript (Part 4)/oddNums.js
@@ -0,0 +1,13 @@
+// in this code we will print odd numbers from 1 to 15 using two different methods
+
+// Method 1: Using for loop with increment of 2
+for(let i = 1; i <= 15; i +=2) {
+  console.log(i);
+}
+
+// Method 2: Using for loop with if condition
+for(let i = 1; i <= 15; i ++) {
+  if(i%2!=0) {
+ console.log(i);
+  }
+}
