cac130d645cf9c3c08b58bdb8239e99155f2873b
JunaidHassan19 <thejunni01@gmail.com>
2026-07-18 11:18:08 +0530

    Q4

--- 05-javascript/JavaScript (Part 4)/Assignment Qs/script.js
+++ 05-javascript/JavaScript (Part 4)/Assignment Qs/script.js
@@ -34,3 +34,13 @@ while(copy2 > 0) {
   copy2 = Math.floor(copy2/10);
 }
 console.log(sum);
+
+// Qs 4. Print the factorial of a number n. [Factorial of a number is the product of all positive integers less than or equal to a given positive integer and denoted by that integer.] Example: 7! (factorial of 7) = 1x2x3x4x5x6x7 = 5040, 5! (factorial of 5) = 1x2x3x4x5 = 120, 3! (factorial of 3) = 1x2x3 = 6, 0! is always 1
+
+let n = prompt("Enter a Number you want to find factorial of:");
+let factorial = 1;
+
+for(let i = 1; i <= n; i ++) {
+  factorial *= i;
+}
+console.log(`Factorial of ${n} is: ${factorial}`);
