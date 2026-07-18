9c6c11dd26d97a6c26dc97db69606139e1c5a02a
JunaidHassan19 <thejunni01@gmail.com>
2026-07-18 12:40:36 +0530

    Nested Objects

--- 05-javascript/JavaScript (Part 5)/nestedObj.js
+++ 05-javascript/JavaScript (Part 5)/nestedObj.js
@@ -18,3 +18,9 @@ console.log(students.junni.marks); // 92
 
 console.log(students["jsx"]["RollNo"]); // 3
 console.log(students.jsx.RollNo); // 3
+
+students.junni.marks = 95; // Updating marks for junni
+console.log(students.junni.marks); // 95
+
+delete students.jai; // Removing the student 'jai'
+console.log(students);
