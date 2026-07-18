0a4ba444ffadd9b0e5830029f85fb166953c210f
JunaidHassan19 <thejunni01@gmail.com>
2026-07-18 09:53:56 +0530

    for-of Loops

---
+++ 05-javascript/JavaScript (Part 4)/forOfLoop.js
@@ -0,0 +1,14 @@
+// for-of Loop is used to iterate over iterable objects like arrays, strings, maps, sets, etc. It provides a simpler and cleaner syntax compared to traditional for loops.
+
+// Example 1: Iterating over an array
+const fruits = ['apple', 'banana', 'cherry','date'];
+
+for (const fruit of fruits) {
+    console.log(fruit);
+}
+
+// Example 2: Iterating over a string
+const greeting = 'Hello';
+for (const char of greeting) {
+    console.log(char);
+}
