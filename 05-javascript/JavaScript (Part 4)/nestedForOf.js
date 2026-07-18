7a6a67c4376416faf0b1e970c88f325855798111
JunaidHassan19 <thejunni01@gmail.com>
2026-07-18 09:58:14 +0530

    Nested for-of Loop

---
+++ 05-javascript/JavaScript (Part 4)/nestedForOf.js
@@ -0,0 +1,11 @@
+let animals = [
+  ["cat", "dog", "hamster"],
+  ["parrot", "canary", "finch"],
+  ["goldfish", "guppy", "betta"]
+];
+
+for(list of animals) {
+  for(animal of list) {
+    console.log(animal);
+  }
+}
