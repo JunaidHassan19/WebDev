d2c18008dab149f2c77e38d87a9ed705f5dd5800
JunaidHassan19 <thejunni01@gmail.com>
2026-07-18 22:02:08 +0530

    PracticeQs

---
+++ 05-javascript/JavaScript (Part 6)/practiceQs.js
@@ -0,0 +1,10 @@
+//Qs 1:
+ function poem() {
+   console.log("Roses are red,");
+   console.log("Violets are blue,");
+   console.log("I'm writing functions,");
+   console.log("And so are you!");
+ };
+
+ poem();
+
