5e98ae7bced1737c6e1fbf510b70e1b7f770df18
JunaidHassan19 <thejunni01@gmail.com>
2026-07-18 22:23:02 +0530

    Functions with Arguments

---
+++ 05-javascript/JavaScript (Part 6)/functionsArguments.js
@@ -0,0 +1,22 @@
+function greet(name) {
+  console.log("Hello, " + name + "!");
+}
+
+greet("Juni");
+
+function add(a, b) {
+  console.log(a + b);
+}
+
+add(5, 3);
+
+function personInfo(name, age) {
+  console.log("Name: " + name + ", Age: " + age);
+}
+
+personInfo("Alice", 30);
+
+
+/*
+Functions with Arguments:  values passed to a function when it is called. These values are used as input for the function's operations.
+*/
