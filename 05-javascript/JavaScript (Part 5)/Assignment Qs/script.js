72352dad626bc359dfcecb60240d488d281f005f
JunaidHassan19 <thejunni01@gmail.com>
2026-07-18 13:52:41 +0530

    Assignment Qs

---
+++ 05-javascript/JavaScript (Part 5)/Assignment Qs/script.js
@@ -0,0 +1,26 @@
+//Qs 1:
+
+let dice = Math.floor(Math.random() * 6) + 1;
+console.log(`The dice shows: ${dice}`);
+
+//Qs 2:
+const car= {
+  name: "Toyota",
+  model: "Corolla",
+  year: 2020,
+  color: "Blue"
+}
+
+console.log(`Car Name: ${car.name}`);
+
+//Qs 3:
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 30,
+  city: "New York"
+};
+
+person.city = "Los Angeles"; // Updating the city property
+
+person.country = "USA"; // Adding a new property
