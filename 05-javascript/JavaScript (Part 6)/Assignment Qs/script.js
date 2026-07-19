75821d795fe2c00548409371ae69ac8826b70392
JunaidHassan19 <thejunni01@gmail.com>
2026-07-19 21:59:56 +0530

    Assignment Qs

---
+++ 05-javascript/JavaScript (Part 6)/Assignment Qs/script.js
@@ -0,0 +1,22 @@
+//Qs 1. Write a JavaScript function that returns array elements larger than a number.
+
+
+
+
+// Qs 2. Write a JavaScript function to extract unique characters from a string. Example: str="abcdabcdefgggh", ans="abcdefgh"
+
+
+
+
+
+// Qs 3: Write a JavaScript function that accepts a list of country names as input and returns the longest country name as output. Example: country=["Australia","Germany","UnitedStatesofAmerica"] output:"UnitedStatesofAmerica"
+
+
+
+// Qs 4. Write a JavaScript function to count the number of vowels in a string argument
+
+
+
+
+
+// Qs 5. Write a JavaScript function to generate a random number within a range (start, end).
