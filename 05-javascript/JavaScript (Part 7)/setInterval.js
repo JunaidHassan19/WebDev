e446d1a9b8ff7c2e1c56acc54c7daf4393053081
JunaidHassan19 <thejunni01@gmail.com>
2026-07-21 11:12:33 +0530

    SetInterval  function

---
+++ 05-javascript/JavaScript (Part 7)/setInterval.js
@@ -0,0 +1,12 @@
+setInterval(()=> {
+  console.log("setInterval is running every 2 seconds");
+}, 2000);
+
+
+/*
+
+SetInterval is a built-in JavaScript function that allows you to execute a specified function repeatedly at fixed time intervals. The first argument is the function to be executed, and the second argument is the time interval in milliseconds.
+
+In this example, the arrow function () => { console.log("setInterval is running every 2 seconds"); } is executed every 2000 milliseconds (or 2 seconds). This means that the message will be logged to the console every 2 seconds indefinitely until the interval is cleared.
+
+*/
