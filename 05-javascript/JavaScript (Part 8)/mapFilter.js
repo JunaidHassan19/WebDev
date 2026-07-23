0aee07abae7295db99cc583590b637868e26d7be
JunaidHassan19 <thejunni01@gmail.com>
2026-07-23 22:10:18 +0530

    Array Methods -map & filter

--- 05-javascript/JavaScript (Part 8)/mapFilter.js
+++ 05-javascript/JavaScript (Part 8)/mapFilter.js
@@ -44,15 +44,15 @@ console.log(cgpa); // Output: [9.4, 8.8, 7.6, 9.2]
 
 // filter(): Creates a new array with all elements that pass the test implemented by the provided function.
 
-// let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// // Using filter to create a new array with only even numbers
-// let evenNumbers = nums.filter((el) => {
-//   return el % 2 === 0;
-// });
+// Using filter to create a new array with only even numbers
+let evenNumbers = nums.filter((el) => {
+  return el % 2 === 0;
+});
 
-// console.log(evenNumbers); // Output: [2, 4, 6, 8, 10]
+console.log(evenNumbers); // Output: [2, 4, 6, 8, 10]
 
-// // Using filter to create a new array with numbers greater than 5
-// let greaterThanFive = nums.filter((el) => el > 5);
-// console.log(greaterThanFive); // Output: [6, 7, 8, 9, 10]
+// Using filter to create a new array with numbers greater than 5
+let greaterThanFive = nums.filter((el) => el > 5);
+console.log(greaterThanFive); // Output: [6, 7, 8, 9, 10]
