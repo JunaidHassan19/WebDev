df9612ab38cab705c0103d6605724531a63f28bc
JunaidHassan19 <thejunni01@gmail.com>
2026-08-03 12:00:26 +0530

    add JavaScript code to log document elements and update text

---
+++ 05-javascript/JavaScript (Part 9)/script.js
@@ -0,0 +1,16 @@
+console.dir(document.all);
+
+console.dir(document.body);
+
+console.dir(document.head);
+
+console.dir(document.title);
+
+console.dir(document.URL);
+
+console.dir(document.all[10]);
+
+console.dir(document.all[8]);
+document.all[8].innerText = "Peter Parker"; // Changing the text of the 8th element in the document to "Peter Parker"
+
+document.all[11].innerText = "Spider-Man    **** Text Changed ****"; // Changing the text of the 11th element in the document to "Spider-Man **** Text Changed ****"
