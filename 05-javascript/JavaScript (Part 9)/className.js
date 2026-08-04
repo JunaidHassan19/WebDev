20418b4be8f020da3cb9ef01a3514c9958847700
JunaidHassan19 <thejunni01@gmail.com>
2026-08-04 21:28:54 +0530

    class name

---
+++ 05-javascript/JavaScript (Part 9)/className.js
@@ -0,0 +1,8 @@
+/*
+
+Selecting elements by class name using JavaScript is a common task in web development. The `getElementsByClassName` method allows you to retrieve all elements that have a specific class name. Here's an example of how to use it:
+
+returns a live HTMLCollection of elements with the specified class name. You can then iterate over this collection to manipulate the elements as needed.
+
+
+*/
