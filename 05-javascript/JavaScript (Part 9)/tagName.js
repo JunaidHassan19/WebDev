9d8f42e5b4f57918a73e412ea925fa9ff9e195d8
JunaidHassan19 <thejunni01@gmail.com>
2026-08-05 10:34:15 +0530

    Selecting elements by TagName

---
+++ 05-javascript/JavaScript (Part 9)/tagName.js
@@ -0,0 +1,10 @@
+/*
+
+Selecting elements by tag name using JavaScript is a common task in web development. The `getElementsByTagName` method allows you to retrieve all elements that have a specific tag name. Here's an example of how to use it:
+
+returns a live HTMLCollection of elements with the specified tag name. You can then iterate over this collection to manipulate the elements as needed.
+
+*/
+
+console.dir(document.getElementsByTagName("img"));
+console.dir(document.getElementsByTagName("p"));
