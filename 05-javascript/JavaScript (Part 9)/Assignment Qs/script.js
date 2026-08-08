c113dd01ea4a4734aede564e8faac287b3989b0d
JunaidHassan19 <thejunni01@gmail.com>
2026-08-08 21:40:52 +0530

    Qs 1

---
+++ 05-javascript/JavaScript (Part 9)/Assignment Qs/script.js
@@ -0,0 +1,9 @@
+// Qs 1:
+let input = document.createElement("input");
+let btn = document.createElement("button");
+btn.innerText = "Click Me";
+
+let body = document.querySelector("body");
+
+body.appendChild(input);
+body.appendChild(btn);
