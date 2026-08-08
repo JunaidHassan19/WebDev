6380ef2621f6f0ff214f8318ace58baef0054cfa
JunaidHassan19 <thejunni01@gmail.com>
2026-08-08 21:44:29 +0530

    Qs 2

--- 05-javascript/JavaScript (Part 9)/Assignment Qs/script.js
+++ 05-javascript/JavaScript (Part 9)/Assignment Qs/script.js
@@ -7,3 +7,7 @@ let body = document.querySelector("body");
 
 body.appendChild(input);
 body.appendChild(btn);
+
+// Qs 2;
+input.setAttribute("placeholder", "username");
+btn.setApttribute("id", "btn");
