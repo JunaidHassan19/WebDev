12cd971c18be45cc09def51c43c14606b1d26663
JunaidHassan19 <thejunni01@gmail.com>
2026-08-08 22:03:19 +0530

    Assignment Qs

--- 05-javascript/JavaScript (Part 9)/Assignment Qs/script.js
+++ 05-javascript/JavaScript (Part 9)/Assignment Qs/script.js
@@ -22,3 +22,10 @@ let h1 = document.createElement("h1");
 h1.innerText = "DOM Practice";
 body.appendChild(h1);
 h1.classList.add("h1Style");
+
+// Qs 5:
+let p = document.createElement("p");
+p.innerText =
+  "Lorem ipsum dolor sit amet consectetur <b>adipisicing</b> elit. Quisquam, quod.";
+body.appendChild(p);
+p.classList.add("pStyle");
