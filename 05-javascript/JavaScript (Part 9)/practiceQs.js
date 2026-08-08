0783f8ac15e61da7d67dc083d4993a2dc522ed3a
JunaidHassan19 <thejunni01@gmail.com>
2026-08-08 20:57:53 +0530

    Qs 3

--- 05-javascript/JavaScript (Part 9)/practiceQs.js
+++ 05-javascript/JavaScript (Part 9)/practiceQs.js
@@ -6,3 +6,11 @@ let body = document.querySelector("body");
 body.appendChild(p);
 
 p.classList.add("red");
+
+// Qs2 :
+let h3 = document.createElement("h3");
+h3.innerText = "hey i'm blue h3!";
+
+body.appendChild(h3);
+
+h3.classList.add("blue");
