c188279ca93d24a4ac9c702d067bdd45a476cc6c
JunaidHassan19 <thejunni01@gmail.com>
2026-08-18 21:24:21 +0530

    DOM Events

--- 05-javascript/JavaScript (Part-10)/domEvents.js
+++ 05-javascript/JavaScript (Part-10)/domEvents.js
@@ -1,5 +1,4 @@
 /*
 DOM Events in JavaScript: 
     Events are actions or occurrences that happen in the browser, such as user interactions (clicks, key presses, mouse movements) or changes in the state of the document (loading, resizing). JavaScript allows you to respond to these events by attaching event listeners to HTML elements.
-
 */
