276c479def2d0e998f7a4ce74a74a5b7b8aed842
JunaidHassan19 <thejunni01@gmail.com>
2026-08-21 11:25:41 +0530

    Random color generator

---
+++ 05-javascript/JavaScript (Part-10)/activity.js
@@ -0,0 +1,25 @@
+let h1 = document.querySelector("h1");
+let button = document.querySelector("button");
+let div = document.querySelector("div");
+
+button.addEventListener("click", function () {
+  div.style.backgroundColor = getRandomRGBColor();
+  updateHeadingColor();
+});
+
+function getRandomRGBColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+function updateHeadingColor() {
+  const currentColor = div.style.backgroundColor;
+  const rgbValues = currentColor.match(/\d+/g);
+  const r = parseInt(rgbValues[0]);
+  const g = parseInt(rgbValues[1]);
+  const b = parseInt(rgbValues[2]);
+  h1.style.color = currentColor;
+  h1.textContent = `Current Background Color: rgb(${r}, ${g}, ${b})`;
+}
