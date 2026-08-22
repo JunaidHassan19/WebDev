b6e0d31e612a0eedc868433932ab3acfb7ddd703
JunaidHassan19 <thejunni01@gmail.com>
2026-08-22 20:51:07 +0530

    JS Call Stack

--- 05-javascript/JavaScript (Part-11)/callStack.js
+++ 05-javascript/JavaScript (Part-11)/callStack.js
@@ -14,3 +14,22 @@ function demo() {
 console.log("Calling demo function");
 demo();
 console.log("Back in the global context after demo function call");
+
+function one() {
+  console.log("Inside one function");
+  return 1;
+}
+
+function two() {
+  console.log("Inside two function and calling one function twice");
+  return one() + one();
+}
+
+function three() {
+  console.log("Inside three function");
+  let ans = two() + one();
+  console.log(ans);
+}
+
+console.log("Calling three function");
+three();
