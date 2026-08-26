226ecc012ddefb53d48fdc0b24b1a18440a7482b
JunaidHassan19 <thejunni01@gmail.com>
2026-08-26 22:41:53 +0530

    Using Axios

---
+++ 05-javascript/JavaScript (Part-13)/axios.js
@@ -0,0 +1,21 @@
+let btn = document.querySelector("button");
+let p = document.querySelector("p");
+
+btn.addEventListener("click", async () => {
+  let fact = await getFacts();
+  p.textContent = fact;
+});
+
+let url = "https://catfact.ninja/fact";
+
+async function getFacts() {
+  try {
+    let res = await axios.get(url);
+    return res.data.fact;
+  } catch (error) {
+    console.error("Error fetching facts:", error);
+    return "Failed to fetch cat fact.";
+  }
+}
+
+getFacts();
