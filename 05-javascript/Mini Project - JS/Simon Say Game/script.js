9993a19002ccb73a2108d4f2af29cd0ace68a5b5
JunaidHassan19 <thejunni01@gmail.com>
2026-08-31 20:38:38 +0530

    Refactor levelUp and checkAns functions for improved game logic in Simon Says Game

--- 05-javascript/Mini Project - JS/Simon Say Game/script.js
+++ 05-javascript/Mini Project - JS/Simon Say Game/script.js
@@ -32,18 +32,36 @@ function userFlash(btn) {
 }
 
 function levelUp() {
+  userSeq = [];
   level++;
   h2.innerText = `Level ${level}`;
 
   let randIdx = Math.floor(Math.random() * 4);
   let randColor = btns[randIdx];
   let rendBtn = document.querySelector(`.${randColor}`);
+  gameSeq.push(randColor);
+  console.log(gameSeq);
   gameFlash(rendBtn);
+}
+
+function checkAns(idx) {
+  if (userSeq[idx] === gameSeq[idx]) {
+    if (userSeq.length == gameSeq.length) {
+      setTimeout(levelUp, 1000);
+    }
+  } else {
+    h2.innerText = "Game Over, Press Any Key to Restart";
+  }
 }
 
 function btnPress() {
   let btn = this;
   userFlash(btn);
+
+  userColor = btn.getAttribute("id");
+  userSeq.push(userColor);
+
+  checkAns(userSeq.length - 1);
 }
 
 let allBtns = document.querySelectorAll(".btn");
