155a35c034a81d5541d14596951734ba44d80f98
JunaidHassan19 <thejunni01@gmail.com>
2026-09-02 21:21:16 +0530

    Add fruit and math modules with exports for apple, banana, mango, and basic arithmetic functions

---
+++ 08-backend 1 (Node.js)/math.js
@@ -0,0 +1,19 @@
+const sum = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const PI = 3.14;
+
+// let result = {
+//   sum: sum,
+//   subtract: subtract,
+//   multiply: multiply,
+//   PI: PI,
+// };
+// module.exports = result;
+
+module.exports = {
+  sum: sum,
+  subtract: subtract,
+  multiply: multiply,
+  PI: PI,
+};
---
+++ 08-backend 1 (Node.js)/script.js
@@ -0,0 +1,9 @@
+// console.log(math);
+// console.log(math.sum(5, 6));
+// console.log(math.subtract(5, 6));
+// console.log(math.multiply(5, 6));
+// console.log(math.PI);
+
+const info = require("./fruits");
+
+console.log(info);
