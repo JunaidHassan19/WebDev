418b9114e880a3ce40eb54a89aa91fd215640192
JunaidHassan19 <thejunni01@gmail.com>
2026-09-02 22:28:01 +0530

    Add figlet integration and project configuration files

--- 08-backend 1 (Node.js)/script.js
+++ 08-backend 1 (Node.js)/script.js
@@ -4,6 +4,17 @@
 // console.log(math.multiply(5, 6));
 // console.log(math.PI);
 
-const info = require("./fruits");
+// const info = require("./fruits");
 
-console.log(info);
+// console.log(info);
+
+let figlet = require("figlet");
+
+figlet("Junaid Hassan", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
